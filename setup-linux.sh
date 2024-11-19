ab64b34d7957dac37cba8421b4faed2f97fe1046
Gordon MacPherson <gordon@gordonite.tech>
2024-11-19 21:41:16 +0000

    You guessed it, another upgrade for MSVC. ThorSVG we love you, MSVC do better.

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -o "MirrorGodotEditorLinux.x86_64" "https://storage.googleapis.com/mirror_native_client_builds/Engine/77066485/MirrorGodotEditorLinux.x86_64"
+curl -o "MirrorGodotEditorLinux.x86_64" "https://storage.googleapis.com/mirror_native_client_builds/Engine/8e137bf6/MirrorGodotEditorLinux.x86_64"
