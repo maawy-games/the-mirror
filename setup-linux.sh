de23a9630a556da25e747e2847b2b7387078e849
Gordon MacPherson <gordon@gordonite.tech>
2024-11-21 15:42:26 +0000

    Upgrade submodule and engine to use the latest crash fixes in the app

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -o "MirrorGodotEditorLinux.x86_64" "https://storage.googleapis.com/mirror_native_client_builds/Engine/8e137bf6/MirrorGodotEditorLinux.x86_64"
+curl -o "MirrorGodotEditorLinux.x86_64" "https://storage.googleapis.com/mirror_native_client_builds/Engine/aac479fc/MirrorGodotEditorLinux.x86_64"
--- setup-mac.sh
+++ setup-mac.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -o "MirrorGodotEditorMac.app.zip" "https://storage.googleapis.com/mirror_native_client_builds/Engine/8e137bf6/MirrorGodotEditorMac.app.zip"
+curl -o "MirrorGodotEditorMac.app.zip" "https://storage.googleapis.com/mirror_native_client_builds/Engine/aac479fc/MirrorGodotEditorMac.app.zip"
